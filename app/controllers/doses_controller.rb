577f62372844c91660298e71f2371a70e53ed0aa
Laure Duhorane <laure.duhorane@lewagon.org>
2020-08-15 17:07:15 -0400

    1st

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save!
+      redirect_to cocktails_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
