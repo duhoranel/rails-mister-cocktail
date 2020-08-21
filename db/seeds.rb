262395815f348f4c174b031d8449e9ce01742b13
Laure Duhorane <laure.duhorane@lewagon.org>
2020-08-20 21:23:04 -0400

    index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -12,3 +13,7 @@ Ingredient.create(name: "mint leaves")
 Cocktail.create!(name: "Gin Tonic")
 Cocktail.create!(name: "Virgin Mojito")
 Cocktail.create!(name: "Sex on the Beach")
+
+file = URI.open('https://res.cloudinary.com/djcwoukgv/image/upload/v1597792123/sample.jpg')
+Cocktail.create!(name: 'Blue Ivy')
+cocktail.photo.attach(io: file, filename: 'sample.jpg', content_type: 'image/jpg')
